c4cdb641429a6bd925d6ceabe05f18f685d6bac3
mamadou jallow <sulsiralumo@gmail.com>
2015-10-05 02:05:40 +0000

    did the admin create use account

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,7 +1,36 @@
-<?php
-/**
- * Created by mamadou.
- * User: mamadou
- * Date: 8/28/2015
- * Time: 6:12 AM
- */ 
+@extends('layouts/admin')
+
+@section('title', 'Page Title')
+
+@section('featured')
+    @parent
+
+@endsection
+
+@section('container')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">Register</div>
+				<div class="panel-body">
+					@if (count($errors) > 0)
+						<div class="alert alert-danger">
+							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+					@endif
+
+					<form class="form-horizontal" role="form" method="POST" action="{!! route('admin.store') !!}">
+                        @include('__partials/signupForm')
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
